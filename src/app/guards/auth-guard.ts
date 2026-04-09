a59e9e8da86a20b1c8345fe5923d46ee143bafc1
Asiya <shaik.asiya.dev@gmail.com>
2026-04-09 16:13:20 +0530

    Implemented logic to connect to backend to login

--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -2,11 +2,11 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const isLoggedIn = localStorage.getItem('loggedin')
+  const isLoggedIn = localStorage.getItem('loggedin')==='true'
 
   const router = inject(Router);
 
-  if(isLoggedIn==='true')
+  if(isLoggedIn)
   {
     return true
   }
--- src/app/pages/auth/login/login.ts
+++ src/app/pages/auth/login/login.ts
@@ -1,38 +1,62 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
 
- constructor(private router:Router){}
+ constructor(private router:Router, private http:HttpClient){}
 
+ loginUrl:string="http://localhost:8088/api/v1/auth/login"
+
+ 
   isLoggedIn = localStorage.getItem('loggedin')
 
   email=new FormControl('')
   password=new FormControl('')
 
+  errorMsg:string=''
+
+  userLogin:any=''
 
   handleLogin()
   {
     // console.log(this.email.value);
     // console.log(this.password.value)
 
-    if(this.email.value=="shaik@gmail.com" && this.password.value=='123')
-    {
-      localStorage.setItem('loggedin', 'true');
-      this.router.navigate(['/dashboard'])
+    this.userLogin = {
+      email : this.email.value,
+     password : this.password.value
     }
+
+    // console.log(this.userLogin)
+
+    this.http.post<any>(this.loginUrl, this.userLogin).subscribe(
+      (response: any) => {
+        // console.log(response);
+
+        localStorage.setItem('loggedin', 'true');
+        this.router.navigate(['/dashboard'])
+      },
+      (error: any) => {
+        console.log("ERROR : " , error)
+
+        this.errorMsg="Invalid email or password"
+      }
+    )
   }
 
   handleLogout()
   {
     localStorage.removeItem('loggedin')
-    this.router.navigate(['/login'])
+      this.router.navigate(['/login'])
+    
   }
 }
